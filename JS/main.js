56236a2b06d0da742d48805992237210d3f3fe89
unknown <mario.junior16725@gmail.com>
2021-03-24 23:53:17 +0200

    update

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 //To create something
-document.getElementById('your id').onclick = function () {document.getElementById('your id').appendChild('<div> <h1> Your element </h1> <div>')}
+var txtInput = document.getElementById('txtInput').value
+document.getElementById("inputButton").onclick = function () {document.getElementById("tasks").appendChild('<div> <h1> ${txtInput} </h1> </div>')};
 
+/*
 //To show or not something
 var myDiv = document.getElementById("myDiv");
     myDiv.onclick = function () {
@@ -9,3 +11,13 @@ var myDiv = document.getElementById("myDiv");
     } else {
       myDiv.style.display = "none";
     }}
+*/
+
+var title = document.querySelector(".tasks .title")
+
+title.addEventListener('click', showDesc)
+
+function showDesc() {
+  var description = document.querySelector(".tasks .desc")
+  description.classList.toggle('descon');
+}
